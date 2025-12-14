1984afa846231c703638081f6eeb5aabd6185bed
Gabriel Casanova <con.casanovaproductions@outlook.com>
2025-12-14 14:22:09 -0300

    Moved game objects to animationcreator screen

--- main.lua
+++ main.lua
@@ -4,38 +4,23 @@ end
 
 -- io.stdout:setvbuf("no") -- Enable printing in debug.
 
--- Import Libraries:
-local g3d = require("libs.g3d")
-
 -- Import Classes:
 local GameController = require("src.GameController")
-local Player = require("src.Player")
-
-_G.GAME_CONTROLLER = GameController()
 ----------------------------------------------------
 
-local Player1
-local background = g3d.newModel("assets/models/sphere.obj", "assets/textures/starfield.png", {0,0,0}, nil, {500,500,500})
-
 function love.load()
-    Player1 = Player()
-    print("aaaaa")
+    _G.GAME_CONTROLLER = GameController()
 end
 
 function love.update(dt)
-    Player1:update(dt)
     _G.GAME_CONTROLLER:update(dt)
-    g3d.camera.firstPersonMovement(dt)
 end
 
 function love.draw()
-    background:draw()
-    Player1:draw()
     _G.GAME_CONTROLLER:draw()
 end
 
 function love.mousemoved(x, y, dx, dy)
-    --g3d.camera.firstPersonLook(dx,dy)
 end
 
 function love.mousepressed(x, y, button, isTouch, presses)
--- src/gui/screens/AnimationCreator.lua
+++ src/gui/screens/AnimationCreator.lua
@@ -1,32 +1,55 @@
 -- Import libraries:
 local Object = require("libs.classic")
+local g3d = require("libs.g3d")
 
--- Import GUI Elements:
+-- Import Scripts:
+-- Import GUI Elements
 local Button = require("src.gui.Button")
+-- Import Game Objects
+local Character = require("src.Character")
 --------------------------------------
 
 local AnimationCreator = Object:extend()
 
 function AnimationCreator:new()
-    self.buttons = {
+    self.background = g3d.newModel("assets/models/sphere.obj", "assets/textures/starfield.png", {0,0,0}, nil, {500,500,500})
+    self.objects = {
+        character = Character()
+    }
+
+    self.gui = {
         testButton = Button()
     }
 end
 
 function AnimationCreator:update(dt)
-    for index, button in pairs(self.buttons) do
+    -- Update GameObjects.
+    for index, object in pairs(self.objects) do
+        object:update(dt)
+    end
+    
+    -- Update GUI.
+    for index, button in pairs(self.gui) do
         button:update(dt)
     end
 end
 
 function AnimationCreator:draw()
-    for index, button in pairs(self.buttons) do
+    -- Draw GameObjects.
+    self.background:draw()
+    for index, object in pairs(self.objects) do
+        object:draw()
+    end
+
+    -- Draw GUI.
+    for index, button in pairs(self.gui) do
         button:draw()
     end
 end
 
 function AnimationCreator:mousepressed(x, y, _button, isTouch, presses)
-    for index, button in pairs(self.buttons) do
+    -- Mouse GUI.
+    for index, button in pairs(self.gui) do
         button:mousepressed(x, y, _button, isTouch, presses)
     end
 end
